cdee96a5169960c9ccfbaca2543944f76d105ed7
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-05-05 16:07:06 -0300

    feat: initialize firebase auth

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCPCqiJXLqd7hBeEVP2GqnqUGFNTW30O-o',
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
+export const auth = getAuth(app)
