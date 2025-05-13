2adef32fe6119f51bb21e227a6c1aca758fa75e9
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-05-13 14:51:28 -0300

    feat: add UserContext

---
+++ src/context/userContext.tsx
@@ -0,0 +1,43 @@
+import { User } from 'firebase/auth'
+import { createContext, ReactNode, useState } from 'react'
+
+interface IUserContext {
+  currentUser: User
+  isAuthenticated: boolean
+  loginUser: (user: User) => void
+  logoutUser: () => void
+}
+
+export const UserContext = createContext<IUserContext>({
+  currentUser: null,
+  isAuthenticated: false,
+  loginUser: () => {},
+  logoutUser: () => {}
+})
+
+interface UserContextProviderProps {
+  children: ReactNode
+}
+
+export default function UserContextProvider({
+  children
+}: UserContextProviderProps) {
+  const [currentUser, setCurrentUser] = useState<User | null>(null)
+
+  const isAuthenticated = currentUser !== null
+
+  function loginUser(user) {
+    setCurrentUser(user)
+  }
+
+  function logoutUser() {
+    setCurrentUser(null)
+  }
+
+  return (
+    <UserContext.Provider
+      value={{ currentUser, isAuthenticated, loginUser, logoutUser }}>
+      {children}
+    </UserContext.Provider>
+  )
+}
