b4cf9e2446bcc5798a33141724454b1feb0a4b69
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-05-14 16:31:53 -0300

    feat: add Loading to CategoriesOverview

--- src/components/CategoriesOverview/CategoriesOverview.tsx
+++ src/components/CategoriesOverview/CategoriesOverview.tsx
@@ -2,15 +2,18 @@ import { useContext, useEffect } from 'react'
 import { CategoryContext } from '../../context/categoryContext'
 import { Container } from './CategoriesOverview.styles'
 import CategoryOverview from '../CategoryOverview/CategoryOverview'
+import Loading from '../Loading/Loading'
 
 export default function CategoriesOverview() {
-  const { categories, fetchCayegories } = useContext(CategoryContext)
+  const { categories, fetchCayegories, isLoading } = useContext(CategoryContext)
 
   useEffect(() => {
     if (categories.length === 0) {
       fetchCayegories()
     }
   }, [])
+
+  if (isLoading) return <Loading />
 
   return (
     <Container>
