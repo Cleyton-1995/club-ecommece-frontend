19adfeb40b67a1226c7ce65078984451d33a4d7f
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-05-23 17:13:39 -0300

    chore: set up redux-thunk middleware in Redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -5,6 +5,7 @@ import persistReducer from 'redux-persist/es/persistReducer'
 import persistStore from 'redux-persist/es/persistStore'
 
 import rootReducer from './root-reducer'
+import { thunk } from 'redux-thunk'
 
 const persistCofig = {
   key: 'root',
@@ -17,7 +18,10 @@ const persistedRoodReducer: typeof rootReducer = persistReducer(
   rootReducer
 )
 
-export const store = createStore(persistedRoodReducer, applyMiddleware(logger))
+export const store = createStore(
+  persistedRoodReducer,
+  applyMiddleware(thunk, logger)
+)
 export const persistedStore = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
