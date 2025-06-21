849f35b0264e983885b3b97d7d8164635d28e3d9
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-06-21 10:24:32 -0300

    test: ensure custon input changes value on user typing

--- src/components/CustomInput/CustomInput.spec.tsx
+++ src/components/CustomInput/CustomInput.spec.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react'
 import CustomInput from './CustomInput'
 import Colors from '../../theme/theme.colors'
+import userEvent from '@testing-library/user-event'
 
 describe('Custom Input', () => {
   it('should render with error if hasError is true', () => {
@@ -21,5 +22,17 @@ describe('Custom Input', () => {
     const input = getByPlaceholderText('Loren Ipsum')
 
     expect(input).toHaveStyle({ border: 'none' })
+  })
+
+  it('should change value when user types', () => {
+    const { getByPlaceholderText, getByDisplayValue } = render(
+      <CustomInput placeholder="Loren Ipsum" hasError={false} />
+    )
+
+    const input = getByPlaceholderText('Loren Ipsum')
+
+    userEvent.type(input, 'Dolar Sit')
+
+    getByDisplayValue('Dolar Sit')
   })
 })
